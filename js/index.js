b73ef97a3106ec799335b457b231b1acf4268df7
Cameron Garner <camerongarner9@gmail.com>
2020-09-28 17:37:17 -0400

    finished the project!

--- js/index.js
+++ js/index.js
@@ -40,3 +40,59 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+let links = document.querySelectorAll("a")
+
+links[0].textContent = "Services"
+links[1].textContent = "Product"
+links[2].textContent = "Vision"
+links[3].textContent = "Features"
+links[4].textContent = "About"
+links[5].textContent = "Contact"
+
+let h1 = document.querySelector("h1")
+let br = document.createElement("br")
+let br2 = document.createElement("br")
+let h1text1 = document.createTextNode("DOM")
+let h1text2 = document.createTextNode("Is")
+let h1text3 = document.createTextNode("Awesome")
+h1.appendChild(h1text1)
+h1.appendChild(br)
+h1.appendChild(h1text2)
+h1.appendChild(br2)
+h1.appendChild(h1text3)
+
+let button = document.querySelector("button")
+button.textContent = "Get Started"
+
+let headerimg = document.getElementById("cta-img")
+headerimg.setAttribute("src", "img/header-img.png")
+
+let allh4 = document.querySelectorAll("h4")
+
+allh4[0].textContent = "Features"
+allh4[1].textContent = "About"
+allh4[2].textContent = "Services"
+allh4[3].textContent = "Product"
+allh4[4].textContent = "Vision"
+allh4[5].textContent = "Contact"
+
+let allp = document.querySelectorAll("p")
+allp[0].textContent = "Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio,in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis."
+allp[1].textContent = "About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, ininterdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis."
+allp[2].textContent = "Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio,in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis."
+allp[3].textContent = "Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, ininterdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis."
+allp[4].textContent = "Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, ininterdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis."
+allp[6].textContent = "1 (888) 888-8888"
+allp[7].textContent = "sales@greatidea.io"
+allp[8].textContent = "Copyright Great Idea! 2018"
+
+let br3 = document.createElement("br")
+let p5text1 = document.createTextNode("123 Way 456 Street")
+let p5text2 = document.createTextNode("Somewhere, USA")
+allp[5].appendChild(p5text1)
+allp[5].appendChild(br3)
+allp[5].appendChild(p5text2)
+
+let midimg = document.getElementById("middle-img")
+midimg.setAttribute("src", "img/mid-page-accent.jpg")
